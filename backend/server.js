723a40ccc95cfe4df9a5ac9e79e12cdc881b0a47
ramirocastrejon <ramiro.castrejon.jr@gmail.com>
2024-10-24 23:46:25 -0500

    v.1.1
    fixes for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,11 @@ app.use(express.json()); //allows us to accept json data in request body
 
 app.use('/api/products', productRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
